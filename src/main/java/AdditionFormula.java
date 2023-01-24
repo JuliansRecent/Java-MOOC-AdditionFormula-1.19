8a57fd253bd8d5238dd96952ee6731dc96951df0
JuliansRecent <juliansrecent@gmail.com>
2023-01-23 16:27:03 -0800

    Module complete. Prompt the user for two number inputs, then print out to the console the addition equation with the result.

--- src/main/java/AdditionFormula.java
+++ src/main/java/AdditionFormula.java
@@ -7,6 +7,14 @@ public class AdditionFormula {
         Scanner scanner = new Scanner(System.in);
 
         // write your program here
+        // Prompt the user for two numbers
+        System.out.println("Give the first number:");
+        int firstNumber = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int secondNumber = Integer.valueOf(scanner.nextLine());
+        int sum = firstNumber + secondNumber;
+        // Print the operation for the user along with the output
+        System.out.println(firstNumber + " + " + secondNumber + " = " + sum);
 
     }
 }
